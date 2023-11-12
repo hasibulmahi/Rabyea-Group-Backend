66a4d88ad6030cbd1d2b0123d0ed1c50c45ed978
Hasibul Mahi <hasibulmahi@gmail.com>
2023-11-12 23:34:20 +0600

    updated cors and header

--- app.js
+++ app.js
@@ -14,10 +14,13 @@ app.use(
   fileUpload({ limits: { fieldSize: 50 * 1024 * 1024 }, useTempFiles: true })
 );
 // app.use(cors({ origin: "*" }));
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", ["*"]);
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
   next();
 });
 
